0f942811c72d397c037c335a035a4c7191d702da
Alexey Vlasyuk <mail@i-alex.net>
2023-04-13 21:11:00 +0300

    Instantiate coins through PUN2

--- Assets/Scripts/Managers/GameSceneManager.cs
+++ Assets/Scripts/Managers/GameSceneManager.cs
@@ -1,6 +1,7 @@
 using AlexeyVlasyuk.MultiplayerTest.PUN2;
 using AlexeyVlasyuk.MultiplayerTest.Views;
 using Cysharp.Threading.Tasks;
+using Photon.Pun;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
@@ -12,7 +13,7 @@ namespace AlexeyVlasyuk.MultiplayerTest
         private int _numCoins = 20;
 
         [SerializeField]
-        private Coin _coinPrefab;
+        private string _coinPrefab;
 
         [SerializeField]
         private Borders _borders;
@@ -28,6 +29,7 @@ namespace AlexeyVlasyuk.MultiplayerTest
         
         private Camera _cam;
         private Player _player;
+        private bool _isTestMode;
         
         private async void Start()
         {
@@ -45,6 +47,7 @@ namespace AlexeyVlasyuk.MultiplayerTest
             else
             {
                 Debug.Log("No current room defined. Scene is running in test mode");
+                _isTestMode = true;
                 roomSeed = PUN2Controller.Instance.GetRandomSeed();
             }
 
@@ -83,7 +86,18 @@ namespace AlexeyVlasyuk.MultiplayerTest
             for (int i = 0; i < _numCoins; i++)
             {
                 var pos = new Vector2(Random.Range(worldBtmLeftCorner.x + Margin, worldTopRightCorner.x - Margin), Random.Range(worldBtmLeftCorner.y + Margin, worldTopRightCorner.y - Margin));
-                var coin = Instantiate(_coinPrefab, pos, Quaternion.identity);
+                Coin coin;
+                if (!_isTestMode)
+                {
+                    var coinObj = PhotonNetwork.InstantiateRoomObject(_coinPrefab, pos, Quaternion.identity);
+                    coin = coinObj.GetComponent<Coin>();
+                }
+                else
+                {
+                    var coinPrefab = Resources.Load<Coin>(_coinPrefab);
+                    coin = Instantiate(coinPrefab, pos, Quaternion.identity);
+                }
+
                 coin.Init(OnCoinPicked);
             }
         }
